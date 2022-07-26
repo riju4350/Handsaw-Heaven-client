948c9621169e61b669047b0dc70e0a6e18f94fc3
riju4350 <riju4350@gmail.com>
2022-07-26 11:25:49 +0600

    react hooks form added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,15 +1,49 @@
 import React from 'react'
+import { useSignInWithGoogle} from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import auth from '../../firebase.init';
 
 export default function Login() {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+  if(user){
+    console.log(user);
+  }
   return (
     <div className='flex justify-center items-center h-screen'>
       <div class="card w-96 bg-base-100 shadow-xl">
   <div class="card-body">
     <h2 class="text-2xl font-bold text-center">Login</h2>
+    {/* react hook form */}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input  />
+      
+      
+      <input {...register("lastName", { required: true })} />
+      {errors.lastName && <p>Last name is required</p>}
+
+      <input {...register("mail", { required: "Email Address is required" })} />
+      <p>{errors.mail?.message}</p>
+      
+      <input type="submit" />
+    </form>
     <div class="divider">OR</div>
-    <button onClick={()=>{}}class="btn btn-outline btn-primary">Continue with Google</button>
+    <button onClick={()=>{signInWithGoogle()}}class="btn btn-outline btn-primary">Continue with Google</button>
   </div>
 </div>
     </div>
   )
 }
+
+{/* <div class="form-control w-full max-w-xs">
+  <label class="label">
+    <span class="label-text">Email</span>
+  </label>
+  <input type="email" name='email' placeholder="email" class="input input-bordered w-full max-w-xs"
+ {...register("email", { required: true })} />
+  <label class="label">
+    {/* <span class="label-text-alt">error message</span> 
+    {errors.email?.type === 'required' && "Email is required"}
+  </label>
+</div> */}
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,50 +1,21 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   // apiKey:process.env.REACT_APP_API_KEY,
-//   // authDomain:process.env.REACT_APP_AUTH_DOMAIN,
-//   // projectId:process.env.REACT_APP_PROJECT_ID,
-//   // storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
-//   // messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   // appId:process.env.REACT_APP_APP_ID
-//   apiKey: "AIzaSyBLJ7tZpNrQbO8DRWfziJ6AK6j9H6Oux5k",
-//   authDomain: "handsaw-heaven.firebaseapp.com",
-//   projectId: "handsaw-heaven",
-//   storageBucket: "handsaw-heaven.appspot.com",
-//   messagingSenderId: "683146259713",
-//   appId: "1:683146259713:web:587682fce43d65a0fd3f00"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-
-// export default auth;
-
 // Import the functions you need from the SDKs you need
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app"; 
-// import { getAuth } from "firebase/auth"; 
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyC83W-KxckRQERoK_FSw2Xg5GKitAu3b3g",
+  authDomain: "handsaw-heaven-cbbe9.firebaseapp.com",
+  projectId: "handsaw-heaven-cbbe9",
+  storageBucket: "handsaw-heaven-cbbe9.appspot.com",
+  messagingSenderId: "43220594224",
+  appId: "1:43220594224:web:42f742e4dc5cadc244c7f5"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//  const auth = getAuth(app);
-export default app;
+const auth = getAuth(app);
+
+export default auth;
