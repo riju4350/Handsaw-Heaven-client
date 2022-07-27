08ad602dbf10d1f9421d8a8a0a31502111a3fa31
riju4350 <riju4350@gmail.com>
2022-07-27 19:25:10 +0600

    navber updated

--- src/Components/Navber/Navber.js
+++ src/Components/Navber/Navber.js
@@ -13,11 +13,16 @@ const Navber = () => {
     <li><Link to="/">Home</Link></li>
     <li><Link to="/services">Services</Link></li>
     <li><Link to="/blogs">Blogs</Link></li>
-    <li><Link to="/about">About</Link></li>
-    <li><Link to="/contact">Contact</Link></li>
-    
-  {user? <button className='btn btn-ghost font-bold' onClick={logout}>Logout</button> : <li><Link to="/login">Login</Link></li> }
-  {user&&<li><Link to="/dashboard">Dashboard</Link></li> }
+    <li><Link to="/about">About</Link></li>        
+    {user && (
+              <>
+                <li><Link to="/addProduct">Add Product</Link></li>
+                <li><Link to="/manageProduct">Remove Product</Link></li>
+                <li><Link to="/myItems">My Orders</Link></li>
+              </>
+            )}
+    {user? <button className='btn btn-ghost font-bold' onClick={logout}>Logout</button> : <li><Link to="/login">Login</Link></li> }
+    {user&&<li><Link to="/dashboard">Dashboard</Link></li> }
     </>
   return (
     <div class="navbar bg-primary px-5 sticky top-0 z-50 ">
